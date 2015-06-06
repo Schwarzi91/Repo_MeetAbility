07cfc68f051a851cd51a950b51d09c75da6fe9a0
Philipp Schwarzbauer <p.schwarzbauer@gmx.net>
2015-06-06 16:01:28 +0200

    add ViewPager in Package 'viewpager'

--- MeetAbility/app/src/main/java/com/example/philipp/meetability/MainActivity.java
+++ MeetAbility/app/src/main/java/com/example/philipp/meetability/MainActivity.java
@@ -3,15 +3,13 @@ package com.example.philipp.meetability;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import viewpager.PageViewActivity;
 
 
 public class MainActivity extends Activity implements View.OnClickListener
@@ -57,7 +55,10 @@ public class MainActivity extends Activity implements View.OnClickListener
             Toast.makeText(this, "Profil anzeigen", Toast.LENGTH_SHORT).show();
 
         else if(v == btShowHistory)
-            Toast.makeText(this, "Historie anzeigen", Toast.LENGTH_SHORT).show();
+        {
+            Intent viewPagerIntent = new Intent(this, PageViewActivity.class);
+            startActivity(viewPagerIntent);
+        }
     }
 
     @Override
---
+++ MeetAbility/app/src/main/java/viewpager/PageViewActivity.java
@@ -0,0 +1,42 @@
+package viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import com.example.philipp.meetability.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Philipp on 06.06.15.
+ */
+public class PageViewActivity extends android.support.v4.app.FragmentActivity
+{
+    MyPageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpager);
+
+        List<Fragment> fragments = getFragments();
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+    }
+
+    public List<Fragment> getFragments()
+    {
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        fragmentList.add(MyFragment.newInstance("Fragment 1"));
+        fragmentList.add(MyFragment.newInstance("Fragment 2"));
+        fragmentList.add(MyFragment.newInstance("Fragment 3"));
+
+        return fragmentList;
+    }
+}
