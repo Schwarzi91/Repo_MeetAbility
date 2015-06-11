00c5e6eabb28b7e2c87b364899010b8410867626
Philipp Schwarzbauer <p.schwarzbauer@gmx.net>
2015-06-11 18:12:01 +0200

    add timePicker to createActivity

--- MeetAbility/app/src/main/java/com/example/philipp/meetability/CreateActivity.java
+++ MeetAbility/app/src/main/java/com/example/philipp/meetability/CreateActivity.java
@@ -1,33 +1,44 @@
 package com.example.philipp.meetability;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.InputType;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class CreateActivity extends Activity
+
+public class CreateActivity extends Activity implements View.OnClickListener
 {
-    Spinner spActivityTypes;
-    Spinner spGender;
+    private Spinner spActivityTypes;
+    private Spinner spGender;
+    private EditText etFromDate;
+    private EditText etToDate;
+    private DatePickerDialog fromDatePicker;
+    private DatePickerDialog toDatePicker;
+    private SimpleDateFormat dateFormatter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        //Spinner
         spActivityTypes = (Spinner) findViewById(R.id.spActivityType);
 
         ArrayAdapter<CharSequence> activityTypeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.activityTypes, android.R.layout.simple_spinner_item);
 
-// Specify the layout to use when the list of choices appears
         activityTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-// Apply the adapter to the spinner
         spActivityTypes.setAdapter(activityTypeAdapter);
 
         spGender = (Spinner) findViewById(R.id.spGender);
@@ -36,31 +47,53 @@ public class CreateActivity extends Activity
                 R.array.gender, android.R.layout.simple_expandable_list_item_1);
 
         spGender.setAdapter(genderAdapter);
+        //Spinner ende
+
+        //Datum
+        etFromDate = (EditText) findViewById(R.id.etFromDate);
+        etFromDate.setInputType(InputType.TYPE_NULL);
+        etToDate = (EditText) findViewById(R.id.etToDate);
+        etToDate.setInputType(InputType.TYPE_NULL);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.GERMANY);
+        etFromDate.setOnClickListener(this);
+        etToDate.setOnClickListener(this);
+        setDateTimeField();
+        //Datum ende
     }
 
+    private void setDateTimeField()
+    {
 
 
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create, menu);
-        return true;
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                etFromDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                etToDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view)
+    {
+        if(view == etFromDate) {
+            fromDatePicker.show();
+        } else if(view == etToDate) {
+            toDatePicker.show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
-    */
 }
