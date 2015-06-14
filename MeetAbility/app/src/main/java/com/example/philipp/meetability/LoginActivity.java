9237a103ff0eaac8691c2c34c018a65ec2e2fe27
Philipp Schwarzbauer <p.schwarzbauer@gmx.net>
2015-06-15 00:12:53 +0200

    update activity_searchresult (momentan aufrufbar über history)

--- MeetAbility/app/src/main/java/com/example/philipp/meetability/LoginActivity.java
+++ MeetAbility/app/src/main/java/com/example/philipp/meetability/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.philipp.meetability;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
