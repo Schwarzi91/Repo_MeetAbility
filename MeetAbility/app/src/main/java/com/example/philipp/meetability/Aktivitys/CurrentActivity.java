499f990237541a240d2fe6dec64f749c04b2aff5
Philipp Schwarzbauer <p.schwarzbauer@gmx.net>
2015-06-23 21:00:26 +0200

    update currentActivity

--- MeetAbility/app/src/main/java/com/example/philipp/meetability/Aktivitys/CurrentActivity.java
+++ MeetAbility/app/src/main/java/com/example/philipp/meetability/Aktivitys/CurrentActivity.java
@@ -1,8 +1,12 @@
 package com.example.philipp.meetability.Aktivitys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -10,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.philipp.meetability.R;
+import com.example.philipp.meetability.viewpager.HistoryPageViewActivity;
 
 public class CurrentActivity extends Fragment
 {
@@ -75,5 +80,32 @@ public class CurrentActivity extends Fragment
 
         return v;
     }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity_all, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            return true;
+        }
+        if (id == R.id.action_userreport) {
+            Intent intent = new Intent(this, HistoryPageViewActivity.class);
+            intent.putExtra("report", "userreport");
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
 
