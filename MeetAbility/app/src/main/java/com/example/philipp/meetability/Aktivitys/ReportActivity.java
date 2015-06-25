a2c127473896cbc0a50815fec49747af6f1b66d5
Philipp Schwarzbauer <p.schwarzbauer@gmx.net>
2015-06-25 18:30:42 +0200

    final app yeah

--- MeetAbility/app/src/main/java/com/example/philipp/meetability/Aktivitys/ReportActivity.java
+++ MeetAbility/app/src/main/java/com/example/philipp/meetability/Aktivitys/ReportActivity.java
@@ -3,6 +3,7 @@ package com.example.philipp.meetability.Aktivitys;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,16 +13,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.philipp.meetability.R;
 
-public class ReportActivity extends Activity implements View.OnClickListener{
+public class ReportActivity extends Activity implements View.OnClickListener {
 
     private Spinner spinnerReport;
     private String reportType;
     private Button btReport;
+    private EditText etBugDescription;
+    private String strReportType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +33,21 @@ public class ReportActivity extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         reportType = getIntent().getExtras().getString("report");
-        spinnerReport = (Spinner) findViewById(R.id.spActivityType);
+        spinnerReport = (Spinner) findViewById(R.id.spReportReason);
+        etBugDescription = (EditText) findViewById(R.id.etBugDescription);
 
-        if(reportType.equals("userreport")) {
+        if (reportType.equals("userreport")) {
             ArrayAdapter<CharSequence> activityReasonUserreportAdapter = ArrayAdapter.createFromResource(this,
                     R.array.reasons_userreport, R.layout.spinner_style);
             activityReasonUserreportAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinnerReport.setAdapter(activityReasonUserreportAdapter);
-        }
-        else if(reportType.equals("bugreport")) {
+            strReportType = "Userreport: ";
+        } else if (reportType.equals("bugreport")) {
             ArrayAdapter<CharSequence> activityReasonBugreportAdapter = ArrayAdapter.createFromResource(this,
                     R.array.reasons_bugreport, R.layout.spinner_style);
             activityReasonBugreportAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinnerReport.setAdapter(activityReasonBugreportAdapter);
+            strReportType = "Bugreport: ";
         }
         btReport = (Button) findViewById(R.id.btReport);
         btReport.setOnClickListener(this);
@@ -86,6 +92,26 @@ public class ReportActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Report gemeldet", Toast.LENGTH_SHORT).show();
+        sendEmail();
+    }
+
+    private void sendEmail() {
+        String[] recipients = {"admin@meetability.de"};
+        Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+        // prompts email clients only
+        email.setType("message/rfc822");
+        email.putExtra(Intent.EXTRA_EMAIL, recipients);
+        email.putExtra(Intent.EXTRA_SUBJECT, strReportType + spinnerReport.getSelectedItem().toString());
+        email.putExtra(Intent.EXTRA_TEXT, etBugDescription.getText().toString());
+
+        try {
+            // the user can choose the email client
+            startActivity(Intent.createChooser(email,"E-Mail Client wählen" ));
+
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getApplicationContext(), "No email client installed.",
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 }
